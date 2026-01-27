0b87d65a9e7c2a19b90a2154d19987cfdcf0f54e
lzx9924 <irwinlzx99.24@ichat.sp.edu.sg>
2026-01-28 00:31:12 +0800

    Update home.dart

--- home.dart
+++ home.dart
@@ -30,6 +30,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('Home', style: TextStyle(fontSize: 20)),
         backgroundColor: Colors.green,
         centerTitle: true,
