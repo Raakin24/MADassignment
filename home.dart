5164614b37a261543a1b094d0b9a2ac102196204
Matin <matinaryan06.24@ichat.sp.edu.sg>
2026-02-04 14:13:21 +0800

    updated home.dart

--- home.dart
+++ home.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'dataservice.dart'; // Import DataService
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   int _selectedIndex = 1;
   bool loading = true; // To show loading spinner until data is fetched
 
@@ -56,81 +58,93 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: loading
-    ? const Center(child: CircularProgressIndicator())
-    : Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
-            child: Text(
-              "Shops near you!",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: localShopData.length,
-              itemBuilder: (context, index) {
-                final shop = localShopData[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/menu',
-                      arguments: shop, // optional, if you want to pass the shop
-                    );
-                  },
-                  child: Card(
-                    elevation: 0,
-                    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(14),
-                      side: BorderSide(color: Colors.grey.shade300),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(
-                            top: Radius.circular(14),
-                          ),
-                          child: Image.asset(
-                            'assets/img/${shop.imageName}',
-                            height: 180,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(12, 10, 12, 2),
-                          child: Text(
-                            shop.shopName,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
-                          child: Text(
-                            shop.shopLocation,
-                            style: const TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                  child: Text(
+                    "Shops near you!",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
                   ),
-                );
-              },
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: localShopData.length,
+                    itemBuilder: (context, index) {
+                      final shop = localShopData[index];
+                      return GestureDetector(
+                        onTap: () {
+                          selectedShop =
+                              shop; // ✅ store chosen shop in DataService
+                          Navigator.pushNamed(context, '/menu');
+                        },
+
+                        child: Card(
+                          elevation: 0,
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.vertical(
+                                  top: Radius.circular(14),
+                                ),
+                                child: Image.asset(
+                                  'assets/img/${shop.imageName}',
+                                  height: 180,
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                  12,
+                                  10,
+                                  12,
+                                  2,
+                                ),
+                                child: Text(
+                                  shop.shopName,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                  12,
+                                  0,
+                                  12,
+                                  12,
+                                ),
+                                child: Text(
+                                  shop.shopLocation,
+                                  style: const TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
